453b58c2a67dd490145f9fa2c59e6179964784cc
pdhoward <patrick.howard@hotmail.com>
2018-08-28 10:56:38 -0400

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,81 +1,155 @@
 import React from 'react'
 import styled from 'styled-components'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook'
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter'
+import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram'
+import faLinkedin from '@fortawesome/fontawesome-free-brands/faLinkedin'
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub'
+import faEnvelope from '@fortawesome/fontawesome-free-regular/faEnvelope'
+import Container from '../components/Container'
 
 const Wrapper = styled.footer`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-between;
-  align-items: flex-start;
-  margin: 0 auto;
-  max-width: ${props => props.theme.sizes.maxWidth};
+  margin-bottom: 0px;
+  color: #0f0f0f;
+  @media screen and (max-width: 767px) {
+    font-size: 12px;
+  }
+  @media screen and (min-width: 767px) {
+    font-size: 12px;
+  }
+  @media screen and (min-width: 1024px) {
+    font-size: 14px;
+  }
+  text-align: center;
+  position: relative;
+  a {
+    color: #525252;
+    &:visited {
+      color: #525252;
+    }
+    &:hover {
+      color: #11abb0;
+    }
+    &:focus {
+      color: #11abb0;
+    }
+  }
 `
 
-const List = styled.ul`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-between;
-  align-items: flex-start;
-  width: 100%;
-  border-top: 1px solid ${props => props.theme.colors.secondary};
-  padding: 1em 0 2em;
-  margin: 0 1.5em;
+const Social = styled.div`
+  margin: 18px 0 30px 0;
+  padding: 0;
+  @media screen and (max-width: 767px) {
+    font-size: 20px;
+  }
+  @media screen and (min-width: 767px) {
+    font-size: 22px;
+  }
+  @media screen and (min-width: 1024px) {
+    font-size: 30px;
+  }
+  li {
+    display: inline-block;
+    margin: 0;
+    padding: 0;
+    @media screen and (max-width: 767px) {
+      margin-left: 14px;
+    }
+    @media screen and (min-width: 767px) {
+      margin-left: 18px;
+    }
+    @media screen and (min-width: 1024px) {
+      margin-left: 42px;
+    }
+    &:first-child {
+      margin-left: 0;
+    }
+    a[href^='mailto:'] {
+      color: #525252;
+      &:visited {
+        color: #525252;
+      }
+      &:hover {
+        color: #11abb0;
+      }
+      &:focus {
+        color: #11abb0;
+      }
+    }
+  }
 `
 
-const Item = styled.li`
-  display: inline-block;
-  padding: 0.25em 0;
-  width: 100%;
-  @media screen and (min-width: ${props => props.theme.responsive.small}) {
-    width: auto;
+const Copyright = styled.div`
+  margin: 0;
+  padding: 0;
+  li {
+    display: inline-block;
+    margin: 0;
+    padding: 0;
+    line-height: 24px;
+    &:before {
+      padding-left: 10px;
+      padding-right: 10px;
+      color: #095153;
+    }
   }
   a {
-    font-weight: 600;
-    transition: all 0.2s;
-    color: ${props => props.theme.colors.base};
-    &:hover {
-      color: ${props => props.theme.colors.highlight};
-    }
-    &:visited {
-      color: ${props => props.theme.colors.base};
-    }
+    text-decoration: none;
+  }
+  strong {
+    font-weight: 700;
   }
 `
 
 const Footer = () => (
-  <Wrapper>
-    <List>
-      <Item>
-        <a
-          href="https://www.contentful.com/"
-          rel="nofollow noopener noreferrer"
-          target="_blank"
-        >
-          <img
-            src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
-            style={{ width: '100px' }}
-            alt="Powered by Contentful"
-          />
-        </a>
-      </Item>
-      <Item>
-        <a
-          href="https://github.com/pdhoward/gatsbystart"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          intelligent web
+  <Container>
+    <Wrapper>
+      <Social>
+        <li>
+          <a href="https://www.facebook.com/fathurmh" target="_blank">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com/fathurmh" target="_blank">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/fathurmh" target="_blank">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/fathurmh" target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.github.com/fathurmh" target="_blank">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:fathurmh@gmail.com" target="_blank">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+        </li>
+      </Social>
+      <Copyright>
+        Built with{' '}
+        <a href="https://www.gatsbyjs.org" target="_blank">
+          <strong>Gatsby</strong>
         </a>{' '}
         by{' '}
-        <a
-          href="https://github.com/pdhoward"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @urban_coder
+        <a href="https://fathurmh.github.io" target="_blank">
+          <strong>Strategic Machines</strong>
         </a>
-      </Item>
-    </List>
-  </Wrapper>
+        <br />
+        <li>&copy; Copyright 2018</li>
+      </Copyright>
+    </Wrapper>
+  </Container>
 )
-
 export default Footer
